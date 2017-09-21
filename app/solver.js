57dcc5369fbd6599bf1110b9013c422b57d9034c
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-09-21 18:30:57 -0400

    cleaned up/updated to es6 solver.js

--- app/solver.js
+++ app/solver.js
@@ -1,121 +1,147 @@
-//note: all of these functions assume a 9x9 board with numerical entries between 0 and 9, inclusive
+/**
+ *  
+ *  This module defines functions to check the validity of and solve a sudoku puzzle. It acts
+ *  on a 9 by 9 2d array of numbers between 0 and 9 inclusive. 0 is considered blank.
+ *
+ *  N.B. the board supplied to these functions is not validated and an invalid board will throw errors
+ *  
+**/ 
 
-//this function solves the board using a recursive backtracking algorithm, it alters the board in place and assumes the board is valid
-function solve(board, row, col) {
-  var res = hasUnassigned(board);
+
+/**
+ *  
+ *  This function solves the board in place, assuming it can be solved (ie it is valid),
+ *  using a recursive backtracking algorithm. 
+ *  
+**/ 
+function solve(board) {
+	let empty = findUnassigned(board);
   
-  //if no cell is unassigned the board is solved
-  if (!res[0]) {
-    return true;
-  }
+	//if no cell is empty the board is solved
+	if (!empty) {
+		return true;
+	}
 
-  //store the coordinates of the first unassigned cell
-  row = res[1];
-  col = res[2];
+	let row = empty[0];
+	let col = empty[1];
 
-  //loop through all possible entries
-  for (var i = 1; i < 10; i++) {
-    if (checkRow(board, row, col, i) && checkColumn(board, row, col, i) && checkGrid(board, row, col, i)) {   
-      //if i is a valid entry, make the move and recursively solve the remaining unassigned cells
-      board[row][col] = i;
-      
-      if (solve(board, row, col)) {                
-        //inserting i at this cell leads to a solution, the board is now solved
-        return true;
-      }
+	//loop through all possible values empty cell could hold
+	for (let i = 1; i < 10; i++) {
 
-      //inserting i at this cell does not lead to a solution, undo the move and try the next number
-      board[row][col] = 0;
-    }
-  }
+		//if i is valid at empty cell, set cell to i
+		if (checkRow(board, row, col, i) && checkColumn(board, row, col, i) && checkGrid(board, row, col, i)) {   
+			board[row][col] = i;
 
-  //if there are unassigned cells but the above loop cannot find a solution one of the previous assignments
-  //must be at fault, we must move backward
-  return false;
+			//recurse on rest of board, if solve returns true we are done
+			if (solve(board)) {                
+				return true;
+			}
+
+			//no solution exists when cell is set to i, unset cell
+			board[row][col] = 0;
+		}
+	}
+
+	//no solution exists with the board as supplied as the argument in this call,
+	//error lies higher in the callstack so return false
+	return false;
 }
 
-//finds the first empty cell, going left to right and up to down
-//returns an array with the first element being a boolean representing if there is an empty cell
-//if there is an empty cell, the next two elements of the array are the x and y coordinate
-function hasUnassigned(board) {
-  for (var i = 0; i < 9; i++) {
-    for (var j = 0; j < 9; j++) {
-      if (board[i][j] == 0) {
-        return [true, i, j];
-      }
-    }
-  }
-  return [false];
+
+//return coordinates of first empty cell found or false if board has no empty cells
+function findUnassigned(board) {
+	for (let i = 0; i < 9; i++) {
+		for (let j = 0; j < 9; j++) {
+			if (board[i][j] == 0) {
+				return [i, j];
+			}
+		}
+	}
+
+	return false;
 }
 
-//returns whether inserting num at cell x of row y yields a valid row
+
+//return whether row y is valid when the cell at row y column x is set to num
 function checkRow(board, y, x, num) {
-  for (var i = 0; i < 9; i++) {
-    if (i != x && board[y][i] == num) {
-      return false;
-    }
-  }
-  return true;
+	for (let i = 0; i < 9; i++) {
+		if (i != x && board[y][i] == num) {
+			return false;
+		}
+	}
+
+	return true;
 }
 
-//returns whether inserting num at cell x of row y yields a valid column
+
+//return whether column x is valid when the cell at row y column x is set to num
 function checkColumn(board, y, x, num) {
-  for (var j = 0; j < 9; j++) {
-    if (j !== y && board[j][x] == num) {
-      return false;
-    }
-  }
-  return true;
+	for (let j = 0; j < 9; j++) {
+		if (j != y && board[j][x] == num) {
+			return false;
+		}
+	}
+
+	return true;
 }
 
-//returns whether inserting num at cell x of row y yields a valid 3x3 subgrid
+
+//return whether the 3x3 subgrid containing the cell at row y column x is valid when that cell is set to num
 function checkGrid(board, y, x, num) {
-  var baseX = 3 * Math.floor(x / 3);
-  var baseY = 3 * Math.floor(y / 3);
+	let baseX = 3 * Math.floor(x / 3);
+	let baseY = 3 * Math.floor(y / 3);
 
-  for (var i = 0; i < 3; i++) {
-    for (var j = 0; j < 3; j++) {
-      if (!(j + baseY == y && i + baseX == x) && board[j + baseY][i + baseX] == num) {
-        return false;
-      }
-    }
-  }
-  return true;
+	for (let i = 0; i < 3; i++) {
+		for (let j = 0; j < 3; j++) {
+			if (!(j + baseY == y && i + baseX == x) && board[j + baseY][i + baseX] == num) {
+				return false;
+			}
+		}
+	}
+
+	return true;
 }
 
-//determines whether the board is valid
+
+//return whether the board is valid
 function isValid(board) {
-  for (var i = 0; i < 9; i++) {
-    var row = board[i].slice();   //slice so that we create a shallow copy, otherwise board will be altered when pass row to validArray()
-    
-    var col = board.map(row => row[i]);
+	for (let i = 0; i < 9; i++) {
+		let row = board[i].slice();
+		let col = board.map(row => row[i]);
 
-    var grid = board.filter((row, idx) => 3 * Math.floor(i / 3) <= idx && idx < 3 * (Math.floor(i / 3) + 1))  //filter out undesired rows
-                    .map(row => row.slice(3 * (i % 3), 3 * (i % 3 + 1)))                                      //reduce each row to desired subsection        
-                    .reduce((arr, row) => arr.concat(row));                                                   //flatten
+		let grid = board
+			.filter((row, idx) => {
+				return 3 * Math.floor(i / 3) <= idx && idx < 3 * (Math.floor(i / 3) + 1);
+			})
+			.map(row => {
+				return row.slice(3 * (i % 3), 3 * (i % 3 + 1));
+			})
+			.reduce((arr, row) => {
+				return arr.concat(row);
+			});
 
-    if (!validArray(row) || !validArray(col) || !validArray(grid)) {
-      return false;
-    }
-  }
+		if (!validArray(row) || !validArray(col) || !validArray(grid)) {
+			return false;
+		}
+	}
 
-  return true;
-
-  //checks if an array representing a row, column, or grid is valid
-  function validArray(arr) {
-    arr.sort((a,b) => a - b);
-
-    for (var i = 0; i < 9; i++) {
-      if (arr[i] == 0) {
-        continue;
-      } else if (arr[i-1] !== undefined && arr[i] <= arr[i-1]) {  //if arr[i] is nonzero and there is a preceding number, arr[i] must be greater
-        return false;
-      }
-    }
-
-    return true;
-  }
+	return true;
 }
+
+
+//returns whether an array representing a row, column, or grid is valid
+function validArray(arr) {
+	return arr
+		.filter(n => n)
+		.reduce((acc, n) => {
+			if (!acc[0] || acc[1].has(n)) {
+				return [false, null];
+			} else {
+				return [true, acc[1].add(n)];
+			}
+		}, [true, new Set()])[0];
+}
+
 
 export { isValid, solve };
 
